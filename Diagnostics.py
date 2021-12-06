0e05f0d9496b631fdbb7685efcc0bebb94548ee8
Ben Wesson <ben.wesson@wandisco.com>
2021-12-06 12:42:30 +0000

    Refactor day3 challenge 1 to remove count

--- Diagnostics.py
+++ Diagnostics.py
@@ -8,16 +8,14 @@ def extractBinaryNumberFromString(inputString):
 def getValueAtPosition(binaryInput, index):
   if index > 11:
     raise IndexError(f"Inputs are 12 bits long, indexed right to left from 0. Provided index was {index}")
-  return (binaryInput & (1 << index)) >> index
+  return (binaryInput & (1 << index)) >> index #Create bit mask, apply bit mask, then remove trailing zeros to find digit at given index
 
 def getFrequencyAtPositions(inputs, inputLength):
   totalsAtPosition=[0] * inputLength
-  count = 0
   for input in inputs:
-    count += 1
     for index in range(inputLength):
       totalsAtPosition[inputLength-1-index] += getValueAtPosition(input, index)
-  return count, totalsAtPosition
+  return totalsAtPosition
 
 def getGammaAndEpsilon(count, frequencies):
   gammaString = ''
@@ -36,10 +34,9 @@ def getGammaAndEpsilon(count, frequencies):
 
 def main():
   inputLength = 12
-  inputs = map(extractBinaryNumberFromString, getLines('input/day3'))
+  inputs = list(map(extractBinaryNumberFromString, getLines('input/day3')))
 
-  [count, frequencies] = getFrequencyAtPositions(inputs, inputLength)
-  [gamma, epsilon] = getGammaAndEpsilon(count, frequencies)
+  [gamma, epsilon] = getGammaAndEpsilon(len(inputs), getFrequencyAtPositions(inputs, inputLength))
   print(f"Power consumption: {gamma * epsilon}")
 
 if __name__ == "__main__":
