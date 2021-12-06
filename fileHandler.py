1ded3033b7e7de82afb1e14de07163fa6a6911fb
Ben Wesson <ben.wesson@wandisco.com>
2021-12-06 11:52:50 +0000

    Update file handler to strip new lines

--- fileHandler.py
+++ fileHandler.py
@@ -1,6 +1,10 @@
+def stripNewLine(line):
+  return line.strip()
+
 def getLines(file):
   lines = [str]
   with open(file, 'r') as raw:
     lines = raw.readlines()
+    lines = map(stripNewLine, lines)
     raw.close()
   return lines
