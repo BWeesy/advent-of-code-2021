3ad3699f0f55aa90bd11758d06df02875a771fc8
Ben Wesson <ben.wesson@wandisco.com>
2021-12-06 11:50:36 +0000

    Day 2 Challenge 2 Success

--- SubmarinePosition.py
+++ SubmarinePosition.py
@@ -4,15 +4,17 @@ class Submarine:
   def __init__(self):
     self.horizontal = 0
     self.depth = 0
+    self.aim = 0
 
   def forward(self, amount):
     self.horizontal += amount
+    self.depth += amount * self.aim
   
   def up(self, amount):
-    self.depth -= amount
+    self.aim -= amount
   
   def down(self, amount):
-    self.depth += amount
+    self.aim += amount
 
   def getPosition(self):
     return self.depth * self.horizontal
@@ -25,12 +27,32 @@ class Submarine:
       self.up(int(amount))
     if(direction == "down"):
       self.down(int(amount))
+    print(f"ins:{direction} {amount} horiz:{self.horizontal} aim:{self.aim} depth:{self.depth}")
 
 def main():
   submarine = Submarine()
   for line in getLines('input/day2.txt'):
     submarine.parseInstruction(line)
   print(submarine.getPosition())
+
+def test():
+  submarine = Submarine()
+  submarine.parseInstruction("forward 1")
+  assert submarine.getPosition() == 0, f"{submarine.getPosition()}"
+  submarine.parseInstruction("down 1")
+  assert submarine.getPosition() == 0, f"{submarine.getPosition()}"
+  submarine.parseInstruction("forward 1")
+  assert submarine.getPosition() == 2, f"{submarine.getPosition()}"
+  submarine.parseInstruction("up 2")
+  assert submarine.getPosition() == 2, f"{submarine.getPosition()}"
+  submarine.parseInstruction("down 1")
+  assert submarine.getPosition() == 2, f"{submarine.getPosition()}"
+  submarine.parseInstruction("forward 1")
+  assert submarine.getPosition() == 3, f"{submarine.getPosition()}"
+  submarine.parseInstruction("forward 1")
+  assert submarine.getPosition() == 4, f"{submarine.getPosition()}"
+  submarine.parseInstruction("down 1")
+  assert submarine.getPosition() == 4, f"{submarine.getPosition()}"
 
 if __name__ == "__main__":
   main()
