47ad78e29a18e14c8ef32a530d705d06411b2998
Ben Wesson <ben.wesson@wandisco.com>
2021-12-08 11:12:21 +0000

    Day 7 Chall 1 Success

--- Day7.py
+++ Day7.py
@@ -1,6 +1,15 @@
-from fileHandler import getLines
+import fileHandler as fh
 
 def main():
+  subLocations = fh.getMappedCommaSeparatedFirstLine('input/day7', int)
+
+  minTotalCost = sum(subLocations)
+  for rendevous in range(min(subLocations), (max(subLocations))):
+    totalFuelCost = sum([abs(location - rendevous) for location in subLocations])
+    if minTotalCost < totalFuelCost:
+      print(f"Rendevous @ {rendevous-1} for totalCost {minTotalCost}")
+      break
+    minTotalCost = totalFuelCost
   return
 
 if __name__ == "__main__":
