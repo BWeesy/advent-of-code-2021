18b43378c427f94e46db0e9fd1dd0f2055e1f193
Ben Wesson <ben.wesson@wandisco.com>
2021-12-08 20:36:11 +0000

    Finished day8 decoder

--- Day8.py
+++ Day8.py
@@ -1,17 +1,15 @@
 import fileHandler as fh
 
-CORRECT_SEQUENCES = {
-'ZERO'  : 0b1110111,
-'ONE'   : 0b0010010,
-'TWO'   : 0b1011101,
-'THREE' : 0b1011011,
-'FOUR'  : 0b0111010,
-'FIVE'  : 0b1101011,
-'SIX'   : 0b1101111,
-'SEVEN' : 0b1010010,
-'EIGHT' : 0b1111111,
-'NINE'  : 0b1111011,
-}
+ZERO = 0 
+ONE = 1
+TWO = 2
+THREE = 3
+FOUR = 4 
+FIVE = 5 
+SIX = 6  
+SEVEN = 7
+EIGHT = 8
+NINE = 9 
 
 CHAR_BINARY_MAP = {
   'a': 6,
@@ -35,32 +33,96 @@ def extractInputOutput(line):
   output = output.split(" ")
   return codes, output
 
+def reverseLookup(dic, value):
+  return list(dic.keys())[list(dic.values()).index(value)]
+
+def isSubset(possibleSub, super):
+  return possibleSub & super == possibleSub
+
 def findEasySequences(code):
   knownCodes = {}
   for element in code:
     length = len(element)
     if length == 2:
-      knownCodes['ONE'] = convertElementToBinary(element)
+      knownCodes[ONE] = convertElementToBinary(element)
     if length == 3:
-      knownCodes['SEVEN'] = convertElementToBinary(element)
+      knownCodes[SEVEN] = convertElementToBinary(element)
     if length == 4:
-      knownCodes['FOUR'] = convertElementToBinary(element)
+      knownCodes[FOUR] = convertElementToBinary(element)
     if length == 7:
-      knownCodes['EIGHT'] = convertElementToBinary(element)
+      knownCodes[EIGHT] = convertElementToBinary(element)
   return knownCodes
+
+def determineNextKnownCode(codes, knownCodes):
+  result = knownCodes
+  for code in codes:
+    binaryCode = convertElementToBinary(code)
+
+    if binaryCode in knownCodes.values():
+      continue
+
+    if len(code) == 6 and FOUR in knownCodes and isSubset(knownCodes[FOUR], binaryCode):
+      result[NINE] = binaryCode
+      return result
+
+    if len(code) == 6 and ONE in knownCodes and isSubset(knownCodes[ONE], binaryCode):
+      result[ZERO] = binaryCode
+      return result
+
+    if len(code) == 6:
+      result[SIX] = binaryCode
+      return result
+
+    if len(code) == 5 and SIX in knownCodes and isSubset(binaryCode, knownCodes[SIX]):
+      result[FIVE] = binaryCode
+      return result
+
+    if len(code) == 5 and ONE in knownCodes and isSubset(knownCodes[ONE], binaryCode):
+      result[THREE] = binaryCode
+      return result
+
+    if len(code) == 5 and SIX in knownCodes and NINE in knownCodes:
+      result[TWO] = binaryCode
+      return result
+  return result
+
+def determineAllKnownCodes(codes):
+  knownCodes = findEasySequences(codes)
+  while len(knownCodes) < 10:
+    knownCodes = determineNextKnownCode(codes, knownCodes)
+  return knownCodes
+
+def convertReadOutToNumber(readout):
+  result = 0
+  for i, digit in enumerate(reversed(readout)):
+    result += digit * pow(10, i)
+  return result
 
 def main():
   codesWithOutputs = fh.getMappedLines('input/day8', extractInputOutput)
   oneFourSevenOrEightSum = 0
 
-  for code, output in codesWithOutputs:
-    knownCodes = findEasySequences(code)
+  for codes, output in codesWithOutputs:
+    knownCodes = findEasySequences(codes)
     outputInBinary = [convertElementToBinary(output) for output in output]
     intersections = [x for x in outputInBinary if x in knownCodes.values()]
+
     oneFourSevenOrEightSum += len(intersections)
-    print(f"{knownCodes} - {outputInBinary} - {intersections}")
-    
+    #print(f"{knownCodes} - {outputInBinary} - {intersections}")
+
   print(f"Intersections {oneFourSevenOrEightSum}")
 
+def test():
+  codesWithOutputs = extractInputOutput('acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf')
+  outputInBinary = [convertElementToBinary(output) for output in codesWithOutputs[1]]
+  knownCodes = determineAllKnownCodes(codesWithOutputs[0])
+
+  print(f"knownCodes - {knownCodes}")
+
+  result = []
+  for element in outputInBinary:
+    result.append(reverseLookup(knownCodes, element))
+  print(convertReadOutToNumber(result))
+
 if __name__ == "__main__":
-  main()
+  test()
